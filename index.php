e852c6c27a42074423dc370194c64be92314e97f
IchsnArrzq <ichsanarrizqi@smkwikrama.sch.id>
2020-12-13 20:09:53 +0700

    mengubahkan file index.php

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h2>Hello World</h2>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit unde dolorem deleniti sequi quibusdam ratione ullam inventore fugiat, voluptas magni vel molestiae assumenda in laborum cumque asperiores quisquam expedita at.</p>
+    <ul>
+        <li>Ichsan Arrizqi</li>
+        <li>11806634</li>
+    </ul>
 </body>
 </html>
