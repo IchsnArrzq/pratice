e02851a2dddc8dc13367a60546532cf7fb19f43c
IchsnArrzq <ichsanarrizqi@smkwikrama.sch.id>
2020-12-13 19:59:43 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Hello World</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit unde dolorem deleniti sequi quibusdam ratione ullam inventore fugiat, voluptas magni vel molestiae assumenda in laborum cumque asperiores quisquam expedita at.</p>
+</body>
+</html>
